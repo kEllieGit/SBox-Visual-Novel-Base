56c383db1c3afa21660291c48c8ca9e29642a179
Sp00n3xJR <Sp00n3xjr@gmail.com>
2023-12-28 20:06:33 +0200

    Move ExecuteAfterLabel() to ScriptPlayer.cs

--- code/VNBase/Systems/ScriptPlayer.Choices.cs
+++ code/VNBase/Systems/ScriptPlayer.Choices.cs
@@ -1,46 +1,23 @@
 using Sandbox;
 using System.Collections.Generic;
-using SandLang;
 
 namespace VNBase;
 
-partial class ScriptPlayer
+sealed partial class ScriptPlayer
 {
 	[Property] public List<string> ActiveDialogueChoices { get; private set; } = new();
 	public static readonly List<string> ContinueChoice = new( new[] { "Continue" } );
 
 	public void ExecuteChoice( int choiceIndex )
 	{
-		if ( _currentLabel == null ) return;
-
-		var choice = _currentLabel.Choices?[choiceIndex];
-		var dialogueEnvironment = GetEnvironment();
-
-		if ( choice == null && _currentLabel.Choices == null )
+		var choice = _currentLabel?.Choices?[choiceIndex];
+		if ( choice == null )
 		{
 			ExecuteAfterLabel();
 		}
-		else if ( choice?.Condition == null || choice.Condition.Execute( dialogueEnvironment ) is Value.NumberValue { Number: > 0 } )
+		else if ( choice.IsAvailable( GetEnvironment() ) )
 		{
 			SetCurrentLabel( _dialogue.DialogueLabels[choice.TargetLabel] );
-		}
-	}
-
-	private void ExecuteAfterLabel()
-	{
-		var afterLabel = _currentLabel.AfterLabel;
-		foreach ( var codeBlock in afterLabel.CodeBlocks )
-		{
-			codeBlock.Execute( GetEnvironment() );
-		}
-
-		if ( afterLabel.TargetLabel == null )
-		{
-			UnloadScript();
-		}
-		else
-		{
-			SetCurrentLabel( _dialogue.DialogueLabels[afterLabel.TargetLabel] );
 		}
 	}
 }
