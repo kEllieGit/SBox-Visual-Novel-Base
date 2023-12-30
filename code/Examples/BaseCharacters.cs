62e83846718e1fe048f6892b467dd25c798b5185
Sp00n3xJR <Sp00n3xjr@gmail.com>
2023-12-30 16:23:49 +0200

    Delete old example scripts, add BaseCharacters.cs for base character definitions.

---
+++ code/Examples/BaseCharacters.cs
@@ -0,0 +1,15 @@
+﻿namespace VNBase.Examples;
+
+// This file contains a bunch of example character definitions.
+
+public class Unknown : CharacterBase
+{
+	public override string Name => "???";
+}
+
+public class John : CharacterBase
+{
+	public override string Name => "John MacAvitch";
+	public override string Title => "Office Employee";
+	public override string Images => "/materials/vnbase/scripts/john";
+}
