27e25ccb454ebdcba8fb0e935ee3c3a3f1b10e10
Sp00n3xJR <Sp00n3xjr@gmail.com>
2024-03-09 16:55:40 +0200

    Remove ExampleCharScript and John examples

--- code/Examples/BaseCharacters.cs
+++ code/Examples/BaseCharacters.cs
@@ -6,10 +6,3 @@ public class Unknown : Character
 {
 	public override string Name => "???";
 }
-
-public class John : Character
-{
-	public override string Name => "John MacAvitch";
-	public override string Title => "Office Employee";
-	public override string Images => "/materials/vnbase/scripts/john";
-}
