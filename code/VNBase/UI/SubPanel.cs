6a26d0c1bbb99b9243bd9f7aee2668c446a3aed3
Sp00n3xJR <Sp00n3xjr@gmail.com>
2024-03-09 16:57:17 +0200

    Update and fix UI Code, reimplement click and hover sounds.

--- code/VNBase/UI/SubPanel.cs
+++ code/VNBase/UI/SubPanel.cs
@@ -6,8 +6,18 @@ public class SubPanel : Panel
 {
 	public ScriptPlayer Player { get; set; }
 
-	public void TogglePanel()
+	public void ToggleVisibility()
 	{
 		SetClass( "hidden", IsVisible );
+	}
+
+	public void Hide()
+	{
+		AddClass( "hidden" );
+	}
+
+	public void Show()
+	{
+		RemoveClass( "hidden" );
 	}
 }
