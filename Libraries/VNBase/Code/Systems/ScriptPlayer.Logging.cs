3978536006eb011f505feeec399637481918f009
Sp00n3xJR <Sp00n3xjr@gmail.com>
2024-11-05 04:10:29 +0200

    add vnbase_logging convar, LoggingEnabled starts off false.

--- Libraries/VNBase/Code/Systems/ScriptPlayer.Logging.cs
+++ Libraries/VNBase/Code/Systems/ScriptPlayer.Logging.cs
@@ -1,10 +1,12 @@
-﻿using Sandbox.Diagnostics;
+﻿using Sandbox;
+using Sandbox.Diagnostics;
 
 namespace VNBase;
 
 public sealed partial class ScriptPlayer
 {
-	public static Logger Log { get => _log; }
-	private readonly static Logger _log = new( "VNBase" );
-	public static bool LoggingEnabled { get; set; } = true;
+	[ConVar("vnbase_logging")]
+	public static bool LoggingEnabled { get; set; } = false;
+
+	public readonly static Logger Log = new( "VNBase" );
 }
