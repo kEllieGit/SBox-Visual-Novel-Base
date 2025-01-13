db2d21e98567ef05a8164a23bab16f7d38b1098b
kEllie <kEllieBusiness2@gmail.com>
2025-01-13 17:52:57 +0200

    Add some useful actions

---
+++ Libraries/VNBase/Code/Systems/ScriptPlayer.Actions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SandLang;
+using VNBase.Assets;
+
+namespace VNBase;
+
+public sealed partial class ScriptPlayer
+{
+	public Action<Script>? OnScriptLoad { get; set; }
+	
+	public Action<Script>? OnScriptUnload { get; set; }
+	
+	public Action<Dialogue.Label>? OnLabelSet { get; set; }
+}
